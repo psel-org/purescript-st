f8d2fc7b9822a63c5221ca3b21085877108ba6af
kamoii <>
2022-01-23 07:00:31 +0900

    Add .el FFI

--- spago.dhall
+++ spago.dhall
@@ -1,6 +1,9 @@
 { name = "st"
-, dependencies = [ "partial", "prelude", "tailrec", "unsafe-coerce" ]
-, packages = https://raw.githubusercontent.com/psel-org/package-sets/main/src/el-0.14.5-20211116/packages.dhall
+, dependencies =
+      [ "partial", "prelude", "tailrec", "unsafe-coerce" ]
+    # [ "console", "effect" ]
+, packages =
+    https://raw.githubusercontent.com/psel-org/package-sets/main/src/el-0.14.5-20211116/packages.dhall
 , backend = "psel"
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
 }
---
+++ src/Control/Monad/ST/Internal.el
@@ -0,0 +1,73 @@
+;; -*- lexical-binding: t; -*-
+
+(require 'Data.Unit)
+
+(defvar Control.Monad.ST.Internal.map_
+  (lambda (f)
+    (lambda (a)
+      (lambda ()
+         (funcall f (funcall a))))))
+
+(defvar Control.Monad.ST.Internal.pure_
+  (lambda (a)
+    (lambda ()
+      a)))
+
+(defvar Control.Monad.ST.Internal.bind_
+  (lambda (a)
+    (lambda (f)
+      (lambda ()
+        (funcall (funcall f (funcall a)))))))
+
+(defvar Control.Monad.ST.Internal.run
+  (lambda (f)
+    (funcall f)))
+
+(defvar Control.Monad.ST.Internal.while
+  (lambda (f)
+    (lambda (a)
+      (lambda ()
+        (while (funcall f)
+          (funcall a))
+        Data.Unit.unit))))
+
+(defvar Control.Monad.ST.Internal.for
+  (lambda (lo)
+    (lambda (hi)
+      (lambda (f)
+        (let ((i lo))
+          (while (< i hi)
+            (funcall (funcall f i)))
+          Data.Unit.unit)))))
+
+(defvar Control.Monad.ST.Internal.foreach
+  (lambda (as)
+    (lambda (f)
+      (lambda ()
+        (seq-do (lambda (a) (funcall (funcall f a))) as)
+        Data.Unit.unit))))
+
+(defvar Control.Monad.ST.Internal.new
+  (lambda (val)
+    (lambda ()
+      (cons val 'Control.Monad.ST.Internal.STRef))))
+
+(defvar Control.Monad.ST.Internal.read
+  (lambda (ref)
+    (lambda ()
+      (car ref))))
+
+(defvar Control.Monad.ST.Internal.modifyImpl
+  (lambda (f)
+    (lambda (ref)
+      (lambda ()
+        (let ((obj (funcall f (car ref))))
+          (setcar ref (psel/alist-get 'state obj))
+          (psel/alist-get 'value obj))))))
+
+(defvar Control.Monad.ST.Internal.write
+  (lambda (val)
+    (lambda (ref)
+      (lambda ()
+        (setcar ref val)
+        Data.Unit.unit))))
